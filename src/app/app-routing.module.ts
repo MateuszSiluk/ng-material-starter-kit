e828a308e59e17510dbda0886e078b0a17db126d
Mateusz Siluk <lowgular.mateusz.siluk@esky.com>
2022-11-03 19:11:55 +0000

    Exercise 3 - crypto list created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,21 +2,27 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { CategoriesListComponent } from './components/categories-list/categories-list.component';
+import { CryptoListComponent } from './components/crypto-list/crypto-list.component';
 import { ProductListComponentModule } from './components/product-list/product-list.component-module';
 import { ProductsServiceModule } from './services/products.service-module';
 import { CategoriesListComponentModule } from './components/categories-list/categories-list.component-module';
 import { CategoriesServiceModule } from './services/categories.service-module';
+import { CryptoListComponentModule } from './components/crypto-list/crypto-list.component-module';
+import { CryptosServiceModule } from './services/cryptos.service-module';
 
 @NgModule({
   imports: [
     RouterModule.forRoot([
       { path: 'products', component: ProductListComponent },
-      { path: 'categories', component: CategoriesListComponent }
+      { path: 'categories', component: CategoriesListComponent },
+      { path: 'crypto', component: CryptoListComponent }
     ]),
     ProductListComponentModule,
     ProductsServiceModule,
     CategoriesListComponentModule,
-    CategoriesServiceModule
+    CategoriesServiceModule,
+    CryptoListComponentModule,
+    CryptosServiceModule
   ],
   exports: [RouterModule],
 })
