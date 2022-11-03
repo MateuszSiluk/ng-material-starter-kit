c54568c276da3d718987361198ff6c87d2523303
Mateusz Siluk <lowgular.mateusz.siluk@esky.com>
2022-11-03 19:44:51 +0000

    Exercise 4 - public holiday list created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,26 +3,32 @@ import { RouterModule } from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { CategoriesListComponent } from './components/categories-list/categories-list.component';
 import { CryptoListComponent } from './components/crypto-list/crypto-list.component';
+import { PublicHolidayListComponent } from './components/public-holiday-list/public-holiday-list.component';
 import { ProductListComponentModule } from './components/product-list/product-list.component-module';
 import { ProductsServiceModule } from './services/products.service-module';
 import { CategoriesListComponentModule } from './components/categories-list/categories-list.component-module';
 import { CategoriesServiceModule } from './services/categories.service-module';
 import { CryptoListComponentModule } from './components/crypto-list/crypto-list.component-module';
 import { CryptosServiceModule } from './services/cryptos.service-module';
+import { PublicHolidayListComponentModule } from './components/public-holiday-list/public-holiday-list.component-module';
+import { PublicHolidaysServiceModule } from './services/public-holidays.service-module';
 
 @NgModule({
   imports: [
     RouterModule.forRoot([
       { path: 'products', component: ProductListComponent },
       { path: 'categories', component: CategoriesListComponent },
-      { path: 'crypto', component: CryptoListComponent }
+      { path: 'crypto', component: CryptoListComponent },
+      { path: 'public-holidays', component: PublicHolidayListComponent }
     ]),
     ProductListComponentModule,
     ProductsServiceModule,
     CategoriesListComponentModule,
     CategoriesServiceModule,
     CryptoListComponentModule,
-    CryptosServiceModule
+    CryptosServiceModule,
+    PublicHolidayListComponentModule,
+    PublicHolidaysServiceModule
   ],
   exports: [RouterModule],
 })
