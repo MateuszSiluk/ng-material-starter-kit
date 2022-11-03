952cd6649b5ba02fccc7d43cf0df42f3fa748843
Mateusz Siluk <lowgular.mateusz.siluk@esky.com>
2022-11-03 19:44:51 +0000

    Exercise 4 - public holiday list created

---
+++ src/app/services/public-holidays.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { PublicHolidayModel } from '../models/public-holiday.model';
+
+const URL_API = 'https://date.nager.at/api/v2/publicholidays/2020/US';
+
+@Injectable()
+export class PublicHolidaysService {
+  constructor(private _httpClient: HttpClient) {}
+
+  getAll(): Observable<PublicHolidayModel[]> {
+    return this._httpClient.get<PublicHolidayModel[]>(`${URL_API}`);
+  }
+}
