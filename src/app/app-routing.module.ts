c63e5c65f974000bc3b6a3b1521f2c0f610e2282
Mateusz Siluk <lowgular.mateusz.siluk@esky.com>
2022-11-06 18:55:18 +0000

    Exercise 3 - product details created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,21 +2,27 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CatFactDetailsComponent } from './components/cat-fact-details/cat-fact-details.component';
 import { AgePredictionDetailsComponent } from './components/age-prediction-details/age-prediction-details.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
 import { CatFactDetailsComponentModule } from './components/cat-fact-details/cat-fact-details.component-module';
 import { CatFactServiceModule } from './services/cat-fact.service-module';
 import { AgePredictionDetailsComponentModule } from './components/age-prediction-details/age-prediction-details.component-module';
 import { AgePredictionServiceModule } from './services/age-prediction.service-module';
+import { ProductDetailsComponentModule } from './components/product-details/product-details.component-module';
+import { ProductsServiceModule } from './services/products.service-module';
 
 @NgModule({
   imports: [
     RouterModule.forRoot([
       { path: 'cat-fact', component: CatFactDetailsComponent },
-      { path: 'age/:name', component: AgePredictionDetailsComponent }
+      { path: 'age/:name', component: AgePredictionDetailsComponent },
+      { path: 'product/:id', component: ProductDetailsComponent }
     ]),
     CatFactDetailsComponentModule,
     CatFactServiceModule,
     AgePredictionDetailsComponentModule,
-    AgePredictionServiceModule
+    AgePredictionServiceModule,
+    ProductDetailsComponentModule,
+    ProductsServiceModule
   ],
   exports: [RouterModule],
 })
