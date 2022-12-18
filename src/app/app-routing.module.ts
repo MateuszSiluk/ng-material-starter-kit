22c50b14f7833061b47ffa2747d4e4d0d8171d14
Mateusz Siluk <lowgular.mateusz.siluk@esky.com>
2022-12-18 22:58:19 +0000

    Exercise 2 - Create filtered list of products based on subject

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { FilteredProductListComponent } from './components/filtered-products-list/filtered-product-list.component';
+import { FilteredProductListBasedOnSubjectComponent } from './components/filtered-product-list-based-on-subject/filtered-product-list-based-on-subject.component';
 import { FilteredProductListComponentModule } from './components/filtered-products-list/filtered-product-list.component-module';
 import { CategoriesServiceModule } from './services/categories.service-module';
 import { ProductsServiceModule } from './services/products.service-module';
+import { FilteredProductListBasedOnSubjectComponentModule } from './components/filtered-product-list-based-on-subject/filtered-product-list-based-on-subject.component-module';
 
 @NgModule({
   imports: [
     RouterModule.forRoot([
-      { path: 'products/:category', component: FilteredProductListComponent },
+      { path: 'products-based-on-routing/:category', component: FilteredProductListComponent },
+      { path: 'products', component: FilteredProductListBasedOnSubjectComponent }
     ]),
     FilteredProductListComponentModule,
     CategoriesServiceModule,
     ProductsServiceModule,
+    FilteredProductListBasedOnSubjectComponentModule
   ],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
